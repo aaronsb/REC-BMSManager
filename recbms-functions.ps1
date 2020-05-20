8dfaaeb4888582a2c224915ae38b580777601866
aaronsb <aaronsb@gmail.com>
2020-05-20 14:18:04 -0700

    Removed autocomplete from get-bmsinstruction

--- recbms-functions.ps1
+++ recbms-functions.ps1
@@ -177,45 +177,7 @@ Function Get-BMSConfigMeta {
 
 Function Get-BMSInstruction {
     [CmdletBinding()]
-    Param($InstructionListArray)
-    DynamicParam {
- 
-        # Set the dynamic parameters' name
-        $ParameterName = 'Instruction'
-
-        # Create the dictionary
-        $RuntimeParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
-
-        # Create the collection of attributes
-        $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
-
-        # Create and set the parameters' attributes
-        $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
-        $ParameterAttribute.Mandatory = $false
-        $ParameterAttribute.Position = 1
-
-        # Add the attributes to the attributes collection
-        $AttributeCollection.Add($ParameterAttribute)
-
-        # Generate and set the ValidateSet
-        # get just instruction names from the library
-        $arrSet = (Get-BMSInstructionList).Instruction
-        $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($arrSet)
-
-        # Add the ValidateSet to the attributes collection
-        $AttributeCollection.Add($ValidateSetAttribute)
-
-        # Create and return the dynamic parameter
-        $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $AttributeCollection)
-        $RuntimeParameterDictionary.Add($ParameterName, $RuntimeParameter)
-        return $RuntimeParameterDictionary
-    }
-
-    begin {
-        #give dynamic parameter a friendly parameter name
-        $Instruction = $PSBoundParameters[$ParameterName]
-        $InstructionList = [ordered]@{}
-    }
+    Param($Instruction)
 
     process {
         #instance instruction library
