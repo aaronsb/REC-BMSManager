d8c1266ffe867c418c98361fadb6235268c14a6a
aaronsb <aaronsb@gmail.com>
2020-05-29 22:45:46 -0700

    Named function libraries better and organized descriptions

--- recbms.ps1
+++ recbms.ps1
@@ -1,13 +1,31 @@
 #manifest
-. .\functions\Assert-BMSMessage.ps1
-. .\functions\Build-BMSMessage.ps1
-. .\functions\Send-BMSMessage.ps1
-. .\functions\Parse-BMSMessage.ps1
-. .\functions\Get-BMSInstructionlist.ps1
-. .\functions\Get-CRC16.ps1
+
+# Preprocessor for determining instruction type input and constructing a valid message
+. .\functions\CMDPreProcessor.ps1
+
+# Instruction message assertion code, generates valid query data structures
+. .\functions\AssertMessage.ps1
+
+# CRC functions for signing byte data streams
+. .\functions\CRC16.ps1
+
+# Hex -> Decimal and Decimal -> Hex converter helpers
 . .\functions\HexConverters.ps1
-. .\functions\Invoke-CMDPreProcessor.ps1
-. .\functions\Sort-MessageStream.ps1
+
+# Instruction message builder to suit instruction command structure syntax
+. .\functions\BuildMessage.ps1
+
+# Serial communication functions for sending and receieving instructions and telemetry
+. .\functions\SendMessage.ps1
+
+# Message parser for converting instruction response bytestreams into value streams
+. .\functions\ParseMessage.ps1
+
+# Stream processor functions for intermediary and final data presentation
+. .\functions\MessageStream.ps1
+
+# Helper functions for simplified UX and presentation
+. .\functions\HelperFunctions.ps1
 
 
 Function Get-BMSLibraryInstance {
