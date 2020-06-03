adc4001056e1b4299a88ed9426839d581a077d71
aaronsb <aaronsb@gmail.com>
2020-06-02 17:21:42 -0700

    Setting up for build-module

--- recbms.ps1
+++ recbms.ps1
@@ -1,28 +1,28 @@
 #manifest
 
 # Preprocessor for determining instruction type input and constructing a valid message
-. .\functions\CMDPreProcessor.ps1
+. .\Private\CMDPreProcessor.ps1
 
 # Instruction message assertion code, generates valid query data structures
-. .\functions\AssertMessage.ps1
+. .\Private\AssertMessage.ps1
 
 # CRC functions for signing byte data streams
-. .\functions\CRC16.ps1
+. .\Private\CRC16.ps1
 
 # Instruction message builder to suit instruction command structure syntax
-. .\functions\BuildMessage.ps1
+. .\Private\BuildMessage.ps1
 
 # Serial communication functions for sending and receieving instructions and telemetry
-. .\functions\SendMessage.ps1
+. .\Private\SendMessage.ps1
 
 # Message parser for converting instruction response bytestreams into value streams
-. .\functions\ParseMessage.ps1
+. .\Private\ParseMessage.ps1
 
 # Stream processor functions for intermediary and final data presentation
-. .\functions\MessageStream.ps1
+. .\Private\MessageStream.ps1
 
 # Helper functions for simplified UX and presentation
-. .\functions\HelperFunctions.ps1
+. .\Public\HelperFunctions.ps1
 
 
 #instance a library global
